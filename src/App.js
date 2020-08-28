4968b751782a4a4f2d23afcc6e11ef5201a7af16
unknown <luissergioguzman56@gmail.com>
2020-08-28 00:15:44 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import Navigation from './components/navigation'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import PageRenderer from './page-renderer'
+
+function App() {
+  const user = {
+    firstName: 'Luis',
+    lastName: 'Coder'
+  }
+  return (
+    <Router>
+      <div className="App">
+        <Navigation user={user}/>
+        <Switch>
+        <Route path="/:page" component={PageRenderer}/>
+        <Route path="/" render={() => <Redirect to="/home"/>}/>
+        <Route component={() => 404}/>
+        </Switch>
+      </div>
+    </Router>
+
+  );
+}
+
+export default App;
---
+++ src/components/navigation.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link } from 'react-router-dom'
+import { Avatar } from 'antd'
+
+const navLinks = [
+    {
+        title: 'Home',
+        path: '/'
+    },
+    {
+        title: 'Blog',
+        path: '/blog'
+    },
+    {
+        title: 'Contact Us',
+        path: '/contact-us'
+    },
+    {
+        title: 'Login',
+        path: '/login'
+    }
+]
+
+export default function Navigation({user}) {
+    return (
+        <nav className="site-navigation">
+            <span className="menu-title">My React Blog</span>
+            <div className="menu-content-container">
+                <ul>
+                    {navLinks.map((link, index) => (
+                        <li key={index}>
+                            <Link to={link.path}>{link.title}</Link>
+                        </li>
+                    ))
+
+                    }
+                </ul>
+                <span>
+                </span>  <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" size={38} />
+                <span className="menu-avatar-name">{`${user.firstName} ${user.lastName}`}</span>
+            </div>
+        </nav>)
+}
