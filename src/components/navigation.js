8b0d2b3f766631dc32cc6597862a6f7c7b5bd012
unknown <luissergioguzman56@gmail.com>
2020-08-29 23:19:17 -0500

    second commit

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from 'react-router-dom'
 import { Avatar } from 'antd'
 
@@ -22,10 +22,11 @@ const navLinks = [
 ]
 
 export default function Navigation({user}) {
+    const [menuActive, setMenuActive] = useState(false)
     return (
         <nav className="site-navigation">
             <span className="menu-title">My React Blog</span>
-            <div className="menu-content-container">
+            <div className={`menu-content-container ${menuActive && 'active'}`}>
                 <ul>
                     {navLinks.map((link, index) => (
                         <li key={index}>
@@ -39,5 +40,6 @@ export default function Navigation({user}) {
                 </span>  <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" size={38} />
                 <span className="menu-avatar-name">{`${user.firstName} ${user.lastName}`}</span>
             </div>
+            <i className="ionicons icon ion-ios-menu" onClick={() => setMenuActive(!menuActive)}></i>
         </nav>)
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './assets/scss/base.scss'
 import 'antd/dist/antd.css';
-import '@quasar/extras/ionicons-v4'
+import '@quasar/extras/ionicons-v4/ionicons-v4.css'
 
 ReactDOM.render(
   <React.StrictMode>
